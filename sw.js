9a87ab73733d21d66e2907e9e207d7dcb75c42e3
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-29 07:45:36 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,52 @@
+const CACHE_NAME = 'catlingo-v1';
+const URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.json',
+  './main.js',
+  './data/lectura-content.js',
+  './data/lectura-generator.js',
+  './data/tips-content.js',
+  './data/tips-generator.js',
+  './data/biblioteca_emojis.json',
+  './data/botiga_emojis.json',
+  './data/categories_emoji.json',
+  './data/minijoc_frases.json',
+  './icon-192.png',
+  './icon-512.png'
+];
 
+// Instal·lació: cachejar tot
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activació: esborrar caches velles
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch: serveix des de cache, si no hi és va a xarxa
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
