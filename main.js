37582ac93ce73c54769b1c924a145119d5d1e38b
samuelsuazo41-afk <samuelsuazo41@gmail.com>
2026-05-29 10:53:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -284,9 +284,17 @@ function mostrarLecturaTab() {
   `;
 }
 
-function generarLectura() {
-  const text = generarTextLectura(1, idioma);
-  document.getElementById('lectura-content').textContent = text;
+function generarTextLectura(nivell, lang) {
+  const D = LECTURA_CONTENT[nivell];
+  let parts = [];
+  parts.push(rand(D.intros));
+  for(let i=0; i<3; i++) {
+    let frase = rand(D.subjectes) + " + rand(D.accions) + " + rand(D.objectes) + " + rand(D.llocs) + ".";
+    parts.push(frase);
+  }
+  parts.push(rand(D.connectors) + ", " + rand(D.subjectes).toLowerCase() + " + rand(D.estats) + ".");
+  parts.push(rand(D.tancaments));
+  return parts.join(" ");
 }
 
 // PESTANYA TIPS
